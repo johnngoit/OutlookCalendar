c9a440bde49fa3a730f883d70587d553affb7b2a
John Ngo <johnngoit@gmail.com>
2023-11-16 22:38:54 -0600

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices.JavaScript;
+using Microsoft.AspNetCore.Mvc;
+using OutlookCalendar.Models;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace OutlookCalendar.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        string credentialsFile = "C:\\dev\\TeamIntegration\\OutlookCalendar\\Files\\credentials.json";
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        /// <summary>
+        /// 7djwrr.onmicrosoft.com
+        /// http://localhost:5007/Home/Privacy
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult OauthRedirect()
+        {
+            JObject credentials = JObject.Parse(System.IO.File.ReadAllText(credentialsFile));
+            var redirectUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?" +
+                "&scope=" + credentials["scopes"].ToString() +
+                "&response_type=code" +
+                "&response_mode=query" +
+                "&state=7djwrr" +
+                "&redirect_uri=" + credentials["redirect_uri"].ToString() +
+                "&client_id=" + credentials["client_id"].ToString();
+            return Redirect(redirectUrl);
+        }
+    }
+}
---
+++ Controllers/OauthController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace OutlookCalendar.Controllers
+{
+    public class OauthController : Controller
+    {
+        string credentialsFile = "C:\\dev\\TeamIntegration\\OutlookCalendar\\Files\\credentials.json";
+        string tokensFile = "C:\\dev\\TeamIntegration\\OutlookCalendar\\Files\\tokens.json";
+        public IActionResult Callback(string code, string state, string error)
+        {
+            JObject credentials = JObject.Parse(System.IO.File.ReadAllText(credentialsFile));
+
+            if (!string.IsNullOrWhiteSpace(code)) { 
+                RestClient restClient = new RestClient("https://login.microsoftonline.com/common/oauth2/v2.0/token");
+                RestRequest request = new RestRequest();
+
+                request.AddParameter("client_id", credentials["client_id"].ToString());
+                request.AddParameter("scope", credentials["scopes"].ToString());
+                request.AddParameter("redirect_uri", credentials["redirect_uri"].ToString());
+                request.AddParameter("code", code); 
+                request.AddParameter("grant_type", "authorization_code");
+                request.AddParameter("client_secret", credentials["client_secret"].ToString());
+                
+                restClient.BuildUri(request);
+                var response = restClient.Post(request);
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    System.IO.File.WriteAllText(tokensFile,response.Content);
+                    return RedirectToAction("Index", "Home");
+                } else
+                {
+                    return RedirectToAction("Error");
+                }
+            }
+            return RedirectToAction("Error");
+        }
+    }
+}
